083b637d7f7136d8bdf9f4827f80f39ebfba9156
Ethan McCullagh <84647308+ethanmccullagh@users.noreply.github.com>
2025-02-03 18:39:17 -0800

    Matching engine (#3)
    * fixed conflicts in requirements.txt
    
    * wrote some code for the engine, fleshed out the basics for storing our orders. Still need to work on DB calls for main logic, as well as the actual matching
    
    ---------
    
    Co-authored-by: David Dennis <daviddennis18@gmail.com>

--- matching-engine/app/core/engine.py
+++ matching-engine/app/core/engine.py
@@ -1,13 +1,31 @@
-from app.schemas.OrderTypes import SellOrder, StockOrder
+from app.schemas.OrderTypes import StockOrder, SellOrder, BuyOrder
+from collections import defaultdict
 
-sellOrders = []
+#TODO: change these to better data structures
+sellTrees = defaultdict(list)
+buyQueues = defaultdict(list)
 
-def receiveOrder (order : StockOrder):
-    return order
+def receiveOrder(order : StockOrder):
+    if order.is_buy:
+        processBuyOrder(BuyOrder(stock_id = order.stock_id, quantity = order.quantity))
+    else
+        processSellOrder(SellOrder(stock_id = order.stock_id, quantity = order.quantity, price = order.price))
+    return {"success": true, "data": null}
+
+
+def getStockPrices():
+    priceList = []
+    for key in sellTrees:
+        priceList.append(sellTrees[key][0])
+    return {"success": true, "data": priceList}
 
 def processSellOrder(sellOrder : SellOrder):
-    sellOrders.append(sellOrder)
-    sellOrders.sort()
+    sellTrees[sellOrder.stock_id].append(sellOrder)
+    sellTrees[sellOrder.stock_id].sort(key=lambda x: x.price)
 
-def processBuyOrder():
-    pass
+def processBuyOrder(buyOrder : BuyOrder):
+    buyOrders[buyOrder.stock_id].append(buyOrder)
+
+
+def cancelOrder(stockID : str):
+    return {}
