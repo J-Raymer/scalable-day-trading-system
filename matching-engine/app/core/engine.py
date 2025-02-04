e1c29f0b4d9fd377c64eaef09f51a4374adcc703
ethanmccullagh <ethan89762@gmail.com>
2025-02-03 18:47:35 -0800

    fixing errors

--- matching-engine/app/core/engine.py
+++ matching-engine/app/core/engine.py
@@ -1,4 +1,4 @@
-from app.schemas.OrderTypes import StockOrder, SellOrder, BuyOrder
+from app.schemas.Types import StockOrder, SellOrder, BuyOrder
 from collections import defaultdict
 
 #TODO: change these to better data structures
@@ -8,23 +8,23 @@ buyQueues = defaultdict(list)
 def receiveOrder(order : StockOrder):
     if order.is_buy:
         processBuyOrder(BuyOrder(stock_id = order.stock_id, quantity = order.quantity))
-    else
+    else:
         processSellOrder(SellOrder(stock_id = order.stock_id, quantity = order.quantity, price = order.price))
-    return {"success": true, "data": null}
+    return {"success": True, "data": None}
 
 
 def getStockPrices():
     priceList = []
     for key in sellTrees:
         priceList.append(sellTrees[key][0])
-    return {"success": true, "data": priceList}
+    return {"success": True, "data": priceList}
 
 def processSellOrder(sellOrder : SellOrder):
     sellTrees[sellOrder.stock_id].append(sellOrder)
     sellTrees[sellOrder.stock_id].sort(key=lambda x: x.price)
 
 def processBuyOrder(buyOrder : BuyOrder):
-    buyOrders[buyOrder.stock_id].append(buyOrder)
+    buyQueues[buyOrder.stock_id].append(buyOrder)
 
 
 def cancelOrder(stockID : str):
