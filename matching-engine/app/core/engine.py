e05bb12e8b7fc4f5726424ca092fcf5670fa238d
ethanmccullagh <ethan89762@gmail.com>
2025-02-08 14:34:39 -0800

    linting

--- matching-engine/app/core/engine.py
+++ matching-engine/app/core/engine.py
@@ -2,36 +2,53 @@ from app.schemas.Types import StockOrder, SellOrder, BuyOrder
 from collections import defaultdict
 from heapq import heapify, heappop, heappush
 
-#TODO: change these to better data structures
+# TODO: change these to better data structures
 sellTrees = []
 buyQueues = defaultdict(list)
-    
-def receiveOrder(order : StockOrder):
+
+
+def receiveOrder(order: StockOrder):
     if order.is_buy:
-        return {"success": True, "data" : processBuyOrder(BuyOrder(stock_id = order.stock_id, quantity = order.quantity)) }
-    return {"success": True, "data" : processSellOrder(SellOrder(stock_id=order.stock_id, quantity=order.quantity, price=order.price))}
+        return {
+            "success": True,
+            "data": processBuyOrder(
+                BuyOrder(stock_id=order.stock_id, quantity=order.quantity)
+            ),
+        }
+    return {
+        "success": True,
+        "data": processSellOrder(
+            SellOrder(
+                stock_id=order.stock_id, quantity=order.quantity, price=order.price
+            )
+        ),
+    }
+
 
 def clearSellOrders():
     global sellTrees
     sellTrees = []
-    return {"message" : "sell orders cleared"}
+    return {"message": "sell orders cleared"}
+
 
 def getStockPrices():
     global sellTrees
 
     stockPrices = [heappop(sellTrees) for i in range(len(sellTrees))]
-    
-    sellTrees = stockPrices
-    return {"success": True, "data" : sellTrees }
 
-def processSellOrder(sellOrder : SellOrder):
+    sellTrees = stockPrices
+    return {"success": True, "data": sellTrees}
+
+
+def processSellOrder(sellOrder: SellOrder):
     heappush(sellTrees, sellOrder)
     return sellOrder
 
-def processBuyOrder(buyOrder : BuyOrder):
+
+def processBuyOrder(buyOrder: BuyOrder):
     buyQueues[buyOrder.stock_id].append(buyOrder)
     return buyOrder
 
 
-def cancelOrder(stockID : str):
+def cancelOrder(stockID: str):
     return {}
--- matching-engine/app/schemas/Types.py
+++ matching-engine/app/schemas/Types.py
@@ -3,33 +3,35 @@ from dataclasses import dataclass
 
 
 class StockOrder(BaseModel):
-    stock_id : str
-    is_buy : bool
-    order_type : str
-    quantity : int
-    price : int
+    stock_id: str
+    is_buy: bool
+    order_type: str
+    quantity: int
+    price: int
 
 
 # TODO: child sell order
 
+
 @dataclass()
 class SellOrder:
-    stock_id : str
-    quantity : int
-    price : int
+    stock_id: str
+    quantity: int
+    price: int
 
-    def __eq__ (self, other):
+    def __eq__(self, other):
         return self.price == other.price
 
-    def __lt__ (self, other):
+    def __lt__(self, other):
         if self.price == other.price:
             return self.stock_id < other.stock_id
         return self.price < other.price
 
 
 class BuyOrder(BaseModel):
-    stock_id : str
-    quantity : int
+    stock_id: str
+    quantity: int
+
 
 class UID(BaseModel):
-    id : str
+    id: str
