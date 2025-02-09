69e86770cf349b056ead904d44bd03969a0627a1
Ethan McCullagh <84647308+ethanmccullagh@users.noreply.github.com>
2025-02-08 17:27:58 -0800

    Matching engine Docker (#5)
    * fixing errors
    
    * added .venv to gitignore
    
    * docker init for matching engine
    
    * cleaning cache files
    
    * sell order processing and heap storage
    
    * small changes to docker build params

--- matching-engine/app/main.py
+++ matching-engine/app/main.py
@@ -1,48 +1,57 @@
 from fastapi import FastAPI
-from app.core.engine import receiveOrder, getPrices, cancelOrder
-from app.schemas.Types import StockOrder,  UID
+from app.core.engine import receiveOrder, getStockPrices, cancelOrder, clearSellOrders
+from app.schemas.Types import StockOrder, UID
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
 
-#engine calls
+# engine calls
 @app.post("/engine/placeStockOrder")
-def placeStockOrder(order : StockOrder):
+def placeStockOrder(order: StockOrder):
     return receiveOrder(order)
 
-@app.post("/transaction/getStockPrices")
-def getStockPrices():
-    return getPrices()
+
+@app.get("/transaction/getStockPrices")
+def getPrices():
+    return getStockPrices()
+
 
 @app.post("/engine/cancelStockTransaction")
-def cancelStockTransaction(stockID : str):
+def cancelStockTransaction(stockID: str):
     return cancelOrder()
 
 
+@app.get("/engine/clearSellOrders")
+def clearOrders():
+    return clearSellOrders()
 
-#DB calls
+
+# DB calls
 @app.post("/transaction/addMoneyToWallet")
-def addMoneyToWallet(amount : int):
+def addMoneyToWallet(amount: int):
     return addMoney(amount)
 
+
 @app.post("/transaction/getWalletBalance")
-def getWalletBalance(id : UID):
+def getWalletBalance(id: UID):
     return getBalance(id)
 
+
 @app.post("/transaction/getStockPortfolio")
-def getStockPortfolio(id : UID):
+def getStockPortfolio(id: UID):
     return getPortfolio(id)
 
+
 @app.post("/transaction/getWalletTransactions")
-def getWalletTransactions(id : UID):
+def getWalletTransactions(id: UID):
     return getWalletLog(id)
 
+
 @app.post("/transaction/getStockTransactions")
-def getStockTransactions(id : UID):
+def getStockTransactions(id: UID):
     return getTransactionsLog(id)
-
-
