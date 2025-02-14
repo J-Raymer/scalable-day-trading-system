df243829ca6e5df1ef0c6fb66618f62eee8ed3db
David Dennis <daviddennis18@gmail.com>
2025-02-14 03:22:51 -0800

    Ok im figuring it out

--- matching-engine/app/core/engine.py
+++ matching-engine/app/core/engine.py
@@ -5,6 +5,7 @@ from schemas.common import SuccessResponse, ErrorResponse, User
 from datetime import datetime 
 from collections import defaultdict, deque
 from heapq import heapify, heappop, heappush
+import requests
 
 sellTrees = defaultdict(list)
 buyQueues = defaultdict(deque)
@@ -48,7 +49,43 @@ def checkMatch():
     for stock_id in sellTrees:
         if (sellTrees[stock_id]) and (buyQueues[stock_id]):
             print('match found')
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
             
+=======
+=======
+>>>>>>> Stashed changes
+            buy_order  = buyQueues[stock_id][0]
+            sell_order = sellTrees[stock_id][0]
+            
+            #get wallet of buyer
+            response = requests.get("http://transaction/getWalletBalance", json={"user": 1})
+           
+            #happy path
+            if buy_order.user_id != sell_order.user_id:
+
+                #happy path
+                if buy_order.quantity == sell_order.quantity:
+
+
+                    # pop both
+                    fufilled_buy  = buyQueues[stock_id].popleft()
+                    fufilled_sell = heappop(sellTrees[stock_id])
+                    # send a transaction to the transaction service
+                    response = requests.post("http://transaction/addMoneyToWallet", json={})
+                         
+
+
+                if buyOrder.quantity < sellOrder.quantity:
+                    pass
+                if buyOrder.quantity > sellOrder.quantity:
+                    pass
+
+
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
             
 
 
@@ -59,26 +96,37 @@ def clearSellOrders():
     return {"message": "sell orders cleared"}
 
 
-'''
 def getStockPrices():
     global sellTrees
-
-    stockPrices = [heappop(sellTrees) for i in range(len(sellTrees))]
-
-    sellTrees = stockPrices
-    return {"success": True, "data": sellTrees}
-'''
+    stockPrices = [(stock_id, sellTrees[stock_id][0]) for stock_id in sellTrees]
+    return {"success": True, "data": stockPrices}
 
 def processSellOrder(sellOrder: SellOrder):
     heappush(sellTrees[sellOrder.stock_id], sellOrder)
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     print(sellTrees[sellOrder.stock_id])
+=======
+    print(sellTrees[sellOrder.stock_id]) #REMOVE AFTER TESTING
+>>>>>>> Stashed changes
+=======
+    print(sellTrees[sellOrder.stock_id]) #REMOVE AFTER TESTING
+>>>>>>> Stashed changes
     checkMatch()
 
 
 
 def processBuyOrder(buyOrder: BuyOrder):
     buyQueues[buyOrder.stock_id].append(buyOrder)
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     print(buyQueues[buyOrder.stock_id])
+=======
+    print(buyQueues[buyOrder.stock_id]) #REMOVE AFTER TESTING
+>>>>>>> Stashed changes
+=======
+    print(buyQueues[buyOrder.stock_id]) #REMOVE AFTER TESTING
+>>>>>>> Stashed changes
     checkMatch()
 
 
