548ef1f28fd1778a0dfead202007b3f2555d8931
J-Raymer <44129402+J-Raymer@users.noreply.github.com>
2025-02-14 00:19:49 -0800

    adds error handling to stocks page

--- frontend/src/routes/StocksPage/StocksPage.tsx
+++ frontend/src/routes/StocksPage/StocksPage.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useStockPrices, Stock } from '@/api/getStockPrices.ts';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Snackbar, Alert, Slide } from '@mui/material';
 import './StocksPage.scss';
 
+interface SlideTransitionProps {
+  children: React.ReactElement;
+  in: boolean;
+  onEnter?: () => void;
+  onExited?: () => void;
+}
+
+function SlideTransition(props: SlideTransitionProps) {
+  return <Slide {...props} direction="up" />;
+}
+
 export const StocksPage = () => {
-  const stocks = useStockPrices();
+  const [error, setError] = useState<string | null>(null);
+  const [open, setOpen] = useState(false);
+
+  const handleError = (message: string) => {
+    setError(message);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setError(null);
+  };
+
+  const stocks = useStockPrices({
+    queryConfig: {
+      onError: (error) => {
+        handleError(error.message);
+      },
+    },
+  });
 
   const columns: GridColDef<Stock>[] = [
     { field: 'stock_id', headerName: 'id' },
@@ -29,9 +59,14 @@ export const StocksPage = () => {
   return (
     <div className="stocks-page">
       <Typography variant="h2">Stocks</Typography>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} TransitionComponent={SlideTransition}>
+        <Alert onClose={handleClose} variant="filled" severity="error">
+          {error}
+        </Alert>
+      </Snackbar>
       <DataGrid
         sx={{ width: 800 }}
-        rows={stocks.data?? []}
+        rows={stocks.data ?? []}
         columns={columns}
         getRowId={(row) => row.stock_id}
         disableRowSelectionOnClick
