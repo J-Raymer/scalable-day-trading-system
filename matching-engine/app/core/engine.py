b2cf383cd4c1c5db3ae8a550d768ad94e18a997e
Ethan McCullagh <84647308+ethan89762@users.noreply.github.com>
2025-02-14 14:22:55 -0800

    started on matching method for processing buy orders. Not finished yet but going with recursive approach for now

--- matching-engine/app/core/engine.py
+++ matching-engine/app/core/engine.py
@@ -1,8 +1,9 @@
+from typing import List
 from fastapi import FastAPI
 from uuid import UUID
 from schemas.engine import StockOrder, SellOrder, BuyOrder
 from schemas.common import SuccessResponse, ErrorResponse, User
-from datetime import datetime 
+from datetime import datetime
 from collections import defaultdict, deque
 from heapq import heapify, heappop, heappush
 
@@ -10,7 +11,6 @@ sellTrees = defaultdict(list)
 buyQueues = defaultdict(deque)
 
 
- 
 def receiveOrder(order: StockOrder, user: User):
     # grab the details only we know
     time = datetime.now()
@@ -18,15 +18,14 @@ def receiveOrder(order: StockOrder, user: User):
 
     if order.is_buy:
         processBuyOrder(
-            BuyOrder(user_id=uid,
-                     stock_id=order.stock_id,
-                     quantity=order.quantity,
-                     timestamp=time)
+            BuyOrder(
+                user_id=uid,
+                stock_id=order.stock_id,
+                quantity=order.quantity,
+                timestamp=time,
+            )
         )
-        return {
-            "success": True,
-            "data": {}
-        }
+        return {"success": True, "data": {}}
     else:
         processSellOrder(
             SellOrder(
@@ -34,23 +33,18 @@ def receiveOrder(order: StockOrder, user: User):
                 stock_id=order.stock_id,
                 quantity=order.quantity,
                 price=order.price,
-                timestamp=time
+                timestamp=time,
             )
         )
-        return {
-            "success": True,
-            "data": {}
-        }
+        return {"success": True, "data": {}}
+
 
 def checkMatch():
     global sellTrees, buyQueues
 
     for stock_id in sellTrees:
         if (sellTrees[stock_id]) and (buyQueues[stock_id]):
-            print('match found')
-            
-            
-
+            print("match found")
 
 
 def clearSellOrders():
@@ -59,7 +53,7 @@ def clearSellOrders():
     return {"message": "sell orders cleared"}
 
 
-'''
+"""
 def getStockPrices():
     global sellTrees
 
@@ -67,13 +61,13 @@ def getStockPrices():
 
     sellTrees = stockPrices
     return {"success": True, "data": sellTrees}
-'''
+"""
+
 
 def processSellOrder(sellOrder: SellOrder):
     heappush(sellTrees[sellOrder.stock_id], sellOrder)
     print(sellTrees[sellOrder.stock_id])
     checkMatch()
-
 
 
 def processBuyOrder(buyOrder: BuyOrder):
@@ -82,6 +76,42 @@ def processBuyOrder(buyOrder: BuyOrder):
     checkMatch()
 
 
+# Matches buy orders to sell orders with partial buy handling
+def matchBuy(buyOrder: BuyOrder, poppedSellOrders: List):
+    global sellTrees
+
+    firstSell = heappop(sellTrees[buyOrder.stock_id])
+
+    # List to store sell orders popped from heap, might be better to create an order_id and just store that
+    poppedSellOrders.append(firstSell)
+
+    buyQuantity = buyOrder.buyQuantity
+    sellQuantity = SellOrder.sellQuantity
+
+    if sellQuantity == buyQuantity:
+        return poppedSellOrders
+
+    # Case where first sell order quantity >= buy order quantity
+    if sellQuantity > buyQuantity:
+
+        # remove buy quantity from sell order
+        firstSell.quantity = firstSell.quantity - buyQuantity
+
+        # push sell order back onto heap with reduced quantity
+        heappush(sellTrees[buyOrder.stock_id], firstSell)
+
+        return poppedSellOrders
+
+    if sellQuantity < buyQuantity:
+
+        buyOrder.quantity = buyOrder.quantity - firstSell.quantity
+
+        return matchBuy(buyOrder, poppedSellOrders)
+
+    # ErrorResponse
+
+    return []
+
+
 def cancelOrder(stockID: str):
     return {}
-
