7a717a2c0af5ed0253ec575515b89692094580b7
J-Raymer <44129402+J-Raymer@users.noreply.github.com>
2025-02-13 20:22:33 -0800

    adds initial trade page layout with call to useStockPortfolio

--- frontend/src/routes/TradePage/TradePage.tsx
+++ frontend/src/routes/TradePage/TradePage.tsx
@@ -1,12 +1,41 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Card, CardContent, Button } from '@mui/material';
+import { useStockPortfolio } from '@/api/getStockPortfolio';
 import './TradePage.scss';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Stock, useStockPrices } from '@/api/getStockPrices';
 
 export function TradePage() {
+  // const stocks = useStockPrices();
+  const stocks = useStockPortfolio();
+  const columns: GridColDef<Stock>[] = [
+    { field: 'stock_id', headerName: 'id' },
+    { field: 'stock_name', headerName: 'Name', flex: 60 },
+    { field: 'price', headerName: 'Price' },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      flex: 15,
+      renderCell: (params) => (
+        <Button
+          onClick={() => console.log(params.row.stock_id)}
+          variant="contained"
+        >
+          Purchase
+        </Button>
+      ),
+    },
+  ];
+
   return (
-    <Container maxWidth="sm" sx={{ mt: 4 }} className="trade-page">
-      <Typography variant="h4" component="h1" gutterBottom>
-        Trade Page
-      </Typography>
-    </Container>
+    <div className="stocks-page">
+      <Typography variant="h2">Stocks</Typography>
+      <DataGrid
+        sx={{ width: 800 }}
+        rows={stocks.data?? []}
+        columns={columns}
+        getRowId={(row) => row.stock_id}
+        disableRowSelectionOnClick
+      />
+    </div>
   );
-}
+};
