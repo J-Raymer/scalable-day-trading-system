2228254c33d701892b914d1cd3b2900b2611da54
Ethan McCullagh <ethan89762@gmail.com>
2025-02-16 17:09:24 -0800

    Squashed commit of the following:
    commit 6cc19a31928cbb09018d683ece0d4e36887576e7
    Author: SteveMKII <67935138+SteveMKII@users.noreply.github.com>
    Date:   Sun Feb 16 11:08:26 2025 -0800
    
        Add proxy headers to fast api (#22)
    
    commit f46c2006bc1413b9a1193297b6011abe4a1df9c6
    Author: SteveMKII <67935138+SteveMKII@users.noreply.github.com>
    Date:   Sun Feb 16 01:15:50 2025 -0800
    
        Wallet Transactions Grid (#21)
    
    commit f8c4fc73fc1a66bdc68fa3f6f4947800229153c8
    Author: SteveMKII <67935138+SteveMKII@users.noreply.github.com>
    Date:   Sun Feb 16 00:52:18 2025 -0800
    
        Cache requirements (#20)
    
    commit bb79c792d1f9721b6c62c9dbda78c90679a01d3c
    Author: SteveMKII <67935138+SteveMKII@users.noreply.github.com>
    Date:   Fri Feb 14 21:55:45 2025 -0800
    
        Refactor frontend components (#12)
    
    commit 5bea3a43a5a8149a04bf6dd776249559683da9a1
    Author: SteveMKII <steve@stevepura.com>
    Date:   Fri Feb 14 21:33:55 2025 -0800
    
        Fix button color in SellStockDialog
    
    commit 9b67d1babc1be2d5a99832a029f08d20eaab301e
    Author: SteveMKII <67935138+SteveMKII@users.noreply.github.com>
    Date:   Fri Feb 14 21:32:27 2025 -0800
    
        Trade page (#11)
    
    commit 49e4a6e87cce64e0b2dfc61c0b69ae83cdcf9e15
    Author: SteveMKII <67935138+SteveMKII@users.noreply.github.com>
    Date:   Fri Feb 14 16:48:14 2025 -0800
    
        Purchase stock dialog (#10)

---
+++ frontend/src/features/transactions/wallet/WalletTransactionsGrid.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { DashboardCard } from '@/components/DashboardCard/DashboardCard.tsx';
+import { Typography } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import {
+  useWalletTransactions,
+  WalletTransaction,
+} from '@/api/getWalletTransactions.ts';
+
+export const WalletTransactionsGrid = () => {
+  const transactions = useWalletTransactions();
+
+  const columns: GridColDef<WalletTransaction>[] = [
+    { field: 'stock_tx_id', headerName: 'Stock Tx Id', flex: 10 },
+    { field: 'wallet_tx_id', headerName: 'Wallet Tx Id', flex: 10 },
+    { field: 'is_debit', headerName: 'Is Debit', flex: 5 },
+    { field: 'amount', headerName: 'Amount', flex: 5 },
+    { field: 'time_stamp', headerName: 'Timestamp', flex: 30 },
+  ];
+
+  const placeHolderData: WalletTransaction[] = [
+    // TODO: Replace this with real data when engine complete
+    // TODO: How should we format the timestamp?
+    {
+      stock_tx_id: 1,
+      wallet_tx_id: 1,
+      is_debit: true,
+      amount: 100,
+      time_stamp: new Date('2023-10-01T10:00:00Z'),
+    },
+    {
+      stock_tx_id: 2,
+      wallet_tx_id: 2,
+      is_debit: false,
+      amount: 200,
+      time_stamp: new Date('2023-10-01T10:00:00Z'),
+    },
+  ];
+
+  return (
+    <DashboardCard>
+      <Typography variant="h5">Wallet Transactions</Typography>
+      <DataGrid
+        columns={columns}
+        rows={placeHolderData}
+        getRowId={(row) => row.wallet_tx_id}
+        disableRowSelectionOnClick
+      />
+    </DashboardCard>
+  );
+};
