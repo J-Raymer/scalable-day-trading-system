d3144d04edb9adf1085743de31c2bf35fbd56ff9
J-Raymer <44129402+J-Raymer@users.noreply.github.com>
2025-02-20 14:11:42 -0800

    update db session for authentication service

---
+++ authentication/db.py
@@ -0,0 +1,18 @@
+import os
+import dotenv
+from sqlmodel import create_engine, Session
+
+dotenv.load_dotenv(override=True)
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+
+# Create the database engine with connection pooling
+engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
