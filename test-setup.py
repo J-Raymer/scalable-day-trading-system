00a6688bd59cf81fac7ef858d19af6ba4a9cf5f8
SteveMKII <steve@stevepura.com>
2025-02-19 21:09:28 -0800

    User database setup

---
+++ test-setup.py
@@ -0,0 +1,54 @@
+import requests
+from faker import Faker
+
+fake = Faker()
+fake.seed_instance(12345)
+
+def register():
+    data = {
+        "user_name": "test",
+        "name": "test",
+        "password": "password"
+    }
+    response = requests.post("http://localhost:3001/authentication/register", json=data, verify=False)
+    return response.json()
+
+def setup_stocks(token: str):
+    stock_data = {
+        "stock_name": fake.company(),
+    }
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.post("http://localhost:3001/setup/createStock", json=stock_data, headers=headers)
+    stock = response.json()
+    return stock
+
+def add_single_stock_to_user(token: str, stock_id: str):
+    stock_data = {
+        "stock_id": stock_id,
+        "quantity":100
+    }
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.post("http://localhost:3001/setup/addStockToUser", json=stock_data, headers=headers)
+    return response.json()
+
+def main():
+
+    try:
+        res = register()
+        token = res['data']['token']
+        results = list(map(lambda _: setup_stocks(token), range(20)))
+        added_stock = add_single_stock_to_user(token,  results[0]['data']['stock_id'])
+        print('Set up user data and stocks')
+        print('User data', res['data'])
+        print('added stock to user', added_stock['data'])
+        print('Stocks', results)
+    except Exception as e:
+        print('Exception occurred setting up tests', e)
+
+
+if __name__ == "__main__":
+    main()
