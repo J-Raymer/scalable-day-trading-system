be4064ab20b5c24009256b1369a50e84a3628e0d
Ethan McCullagh <ethan89762@gmail.com>
2025-03-03 19:14:14 -0800

    stable test implementation

--- rmqtest/app.py
+++ rmqtest/app.py
@@ -1,6 +1,8 @@
 import asyncio
 import aio_pika
 import json
+import logging
+import sys
 
 
 async def process_task(message):
@@ -11,12 +13,26 @@ async def process_task(message):
 
     # Process the task (your business logic here)
     print(task_data)
+    sys.stdout.flush()
 
 
 async def main():
+    # logging.basicConfig(level=logging.DEBUG)
     # Connect to RabbitMQ
     print("rmq test init")
-    connection = await aio_pika.connect_robust("amqp://guest:guest@rabbitmq:5672/")
+
+    attempts = 0
+    try:
+        connection = await aio_pika.connect_robust("amqp://guest:guest@rabbitmq:5672/")
+    except:
+        print("connection failed, retrying")
+        await asyncio.sleep(10)
+        attempts += 1
+
+        if attempts > 10:
+            print("connection failed")
+            return
+
     async with connection:
         channel = await connection.channel()
 
@@ -25,6 +41,8 @@ async def main():
 
         # Start consuming
         await queue.consume(process_task)
+
+        sys.stdout.flush()
 
         await asyncio.sleep(10)
 
