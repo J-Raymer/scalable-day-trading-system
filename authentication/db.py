88fec560b7cc70475c0fe720ad8a2072f498f792
David Dennis <daviddennis18@gmail.com>
2025-03-05 22:08:56 -0800

    WIP for async db changes, apigateway seems fixed but don't know for sure

--- authentication/db.py
+++ authentication/db.py
@@ -1,6 +1,7 @@
 import os
 import dotenv
-from sqlmodel import create_engine, Session
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 dotenv.load_dotenv(override=True)
 USERNAME = os.getenv("USERNAME")
@@ -8,11 +9,13 @@ PASSWORD = os.getenv("PASSWORD")
 HOST = os.getenv("HOST")
 PORT = os.getenv("POSTGRES_PORT")
 DB_NAME = os.getenv("DB_NAME")
-DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
 
 # Create the database engine with connection pooling
-engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-def get_session():
-    with Session(engine) as session:
+
+async def get_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
         yield session
