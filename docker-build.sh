eda35a574dd0566a61dba1d15871f58fdd2957b0
SteveMKII <steve@stevepura.com>
2025-03-06 20:27:59 -0800

    Update build script

--- docker-build.sh
+++ docker-build.sh
@@ -4,7 +4,7 @@
 docker swarm init
 
 # Build all the services
-services=("authentication" "transaction" "database" "matching-engine" "api-gateway" "frontend")
+services=("auth" "transaction" "database" "matching-engine" "api-gateway" "frontend")
 
 for service in "${services[@]}"; do
   echo "Building $service..."
