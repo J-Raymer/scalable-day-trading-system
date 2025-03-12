87f2d286e501c85b5b7c4ed6de1af9d985f30749
SteveMKII <steve@stevepura.com>
2025-03-11 21:00:57 -0700

    Add stock id

--- transaction/main.py
+++ transaction/main.py
@@ -259,6 +259,7 @@ async def add_stock_to_user(new_stock: StockSetup, x_user_data: str = Header(Non
     session.add(stock_portfolio)
     await session.commit()
 
+    # We need to get the stock name because it is required in getStockPortfolio
     cache_hit = cache.get(f'{CacheName.STOCKS}:{StockSetup.stock_id}')
     if cache_hit:
         portfolio_item = {
