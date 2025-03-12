90e27df47bdb43e358720ed5960dadfa6098ff7f
J-Raymer <44129402+J-Raymer@users.noreply.github.com>
2025-03-11 21:18:05 -0700

    fix bug updating cache

--- transaction/main.py
+++ transaction/main.py
@@ -260,7 +260,7 @@ async def add_stock_to_user(new_stock: StockSetup, x_user_data: str = Header(Non
     await session.commit()
 
     # We need to get the stock name because it is required in getStockPortfolio
-    cache_hit = cache.get(f'{CacheName.STOCKS}:{StockSetup.stock_id}')
+    cache_hit = cache.get(f'{CacheName.STOCKS}:{new_stock.stock_id}')
     if cache_hit:
         portfolio_item = {
             user_id: {
