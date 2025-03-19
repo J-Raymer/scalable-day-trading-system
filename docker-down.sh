2d398f9d30a767c6df0b05aee92a1904149d8bee
SteveMKII <steve@stevepura.com>
2025-03-19 15:46:56 -0700

    Update requirements files

--- docker-down.sh
+++ docker-down.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+# build.sh
 
 # Leave the swarm
 docker swarm leave --force
 
 # Nuke the volumes
 docker volume rm daytrader_db_data
-docker volume rm daytrader_redis_data
+#docker volume rm daytrader_redis_data
 
