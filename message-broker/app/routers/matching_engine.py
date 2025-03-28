d479c446f4813cb8ad30de7e057c76712bc704d2
Ethan McCullagh <ethan89762@gmail.com>
2025-03-27 17:26:19 -0700

    refactoring

--- message-broker/app/routers/matching_engine.py
+++ message-broker/app/routers/matching_engine.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Header, HTTPException
 from schemas.common import ErrorResponse, SuccessResponse, RabbitError
-from schemas.engine import StockOrder
+from schemas.engine import StockOrder, CancelOrder
 from ..core.broker import *
 
 router = APIRouter()
@@ -27,6 +27,46 @@ async def placeStockOrder(order: StockOrder, x_user_data: str = Header(None)):
         {"user_id": user_id},
         "matching-engine",
     )
+
+    if response.content_type == "SUCCESS":
+
+        return SuccessResponse.model_validate_json(response.body.decode())
+
+    error = RabbitError.model_validate_json(response.body.decode())
+    raise HTTPException(status_code=error.status_code, detail=error.detail)
+
+
+@router.post(
+    "/cancelStockTransaction",
+    responses={
+        200: {"model": SuccessResponse},
+        400: {"model": ErrorResponse},
+        404: {"model": ErrorResponse},
+    },
+)
+async def cancelStockOrder(order: CancelOrder, x_user_data: str = Header(None)):
+    if not x_user_data:
+        raise HTTPException(status_code=400, detail="User data is missing in headers")
+    username, user_id = x_user_data.split("|")
+
+    response = await rpcCall(
+        order.model_dump_json().encode(),
+        "CANCEL_ORDER",
+        {"user_id": user_id},
+        "matching-engine",
+    )
+
+    if response.content_type == "SUCCESS":
+        return SuccessResponse.model_validate_json(response.body.decode())
+
+    error = RabbitError.model_validate_json(response.body.decode())
+    raise HTTPException(status_code=error.status_code, detail=error.detail)
+
+
+@router.get("/getStockPrices")
+async def getStockPrice():
+
+    response = await rpcCall("".encode(), "GET_PRICES", None, "matching-engine")
 
     if response.content_type == "SUCCESS":
 
